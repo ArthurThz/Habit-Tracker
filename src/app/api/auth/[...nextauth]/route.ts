e8d7de2d87dc10bd7d7b7a5c58486bf50b9dd315
Arthur Theodoro <theodorodev@outlook.com>
2025-06-18 09:02:11 -0300

    (fix) tasks routes are now protected, middleware file it was in the wrong place, now is inside src folder

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -50,14 +50,12 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async jwt({ token, user }) {
-      // Quando o usuário faz login, o 'user' estará disponível
       if (user) {
-        token.id = user.id; // adiciona id ao token JWT
+        token.id = user.id; // add id to token
       }
       return token;
     },
     async session({ session, token }) {
-      // Adiciona o id do token no objeto session.user
       if (token && session.user) {
         session.user.id = token.id as string;
       }
@@ -66,7 +64,6 @@ export const authOptions: NextAuthOptions = {
   },
   pages: {
     signIn: "/login",
-    error: "/login", // agora você vê o erro como query param
   },
 };
 const handler = NextAuth(authOptions);
